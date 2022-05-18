cba912ac8673ff44baf7acec32aa1219406ac990
Deyvi Jara <djara@baufest.com>
2022-05-17 22:35:56 -0500

    Start Project with Git

---
+++ src/main/java/com/udemy/Main.java
@@ -0,0 +1,8 @@
+package com.udemy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Learning Fundamentals with Java 11 !");
+    }
+}
