5e17966bb63f1ec089f0aa1aa21ffb1856d9c494
Deyvi E. Jara Garcia <deyvi.jara@rappi.com>
2022-06-21 09:50:23 -0500

    Section 14: Fundamentos -> Encapsulamiento (setters,getter and toString())

--- src/main/java/com/udemy/entities/Persona.java
+++ src/main/java/com/udemy/entities/Persona.java
@@ -2,12 +2,62 @@ package com.udemy.entities;
 
 public class Persona {
     // Atributos
-    public String nombre;
-    public String apellido;
+    private String nombre;
+    private String apellido;
+    private boolean activo;
+    private Double sueldo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
 
     // Métodos de la clase
+    public Persona(){}
+    public Persona(String nombre, Double sueldo, boolean activo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.activo = activo;
+    }
     public void desplegarInformacion(){
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Apellido: " + this.apellido);
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", activo=" + activo +
+                ", sueldo=" + sueldo +
+                '}';
     }
 }
--- src/main/java/com/udemy/variablespasoporvaloryreferencia/PasoPorReferencia.java
+++ src/main/java/com/udemy/variablespasoporvaloryreferencia/PasoPorReferencia.java
@@ -5,16 +5,21 @@ import com.udemy.entities.Persona;
 public class PasoPorReferencia {
 
     public static void main(String[] args) {
-       Persona persona_1 = new Persona();
-       persona_1.nombre = "Juan";
+       Persona persona_1 = new Persona("David",500.0,true);
+       persona_1.setNombre("Juan");
+        System.out.println("persona1 : " + persona_1.toString());
+        System.out.println("persona1 Nombre: " + persona_1.getNombre());
 
-        System.out.println("persona1 Nombre: " + persona_1.nombre);
         cambiarValor(persona_1); // persona_1, almacena el valor de la referencia del objeto que se creo.
-        System.out.println("persona1 cambio de Nombre: " + persona_1.nombre);
+        System.out.println("persona1 cambio de Nombre: " + persona_1.getNombre());
+        System.out.println("persona1 : " + persona_1.toString());
     }
 
     public static void cambiarValor(Persona persona){
         // El cambio de valores por paso de referencia aplica para objetos.
-        persona.nombre = "Karla";
+        persona.setNombre("Karla");
+        persona.setActivo(false);
+        persona.setApellido("Palacios");
+        persona.setSueldo(1500.);
     }
 }
