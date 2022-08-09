6fa4a0ebe39472fbbc4c32ac08a6acd20ed46c1e
Deyvi E. Jara Garcia <deyvi.jara@rappi.com>
2022-08-08 21:49:11 -0500

    Programacion - Bloques Inicializacion de las Clases

---
+++ src/main/java/com/udemy/programacion/bloquesinicializacion/Persona.java
@@ -0,0 +1,34 @@
+package com.udemy.programacion.bloquesinicializacion;
+
+public class Persona {
+    private final int idPersona;
+    private static int contadorPersonas;
+
+    // Se ejecuta al crear en memoria
+    static {
+        System.out.println("Ejecucion Bloque estatico");
+        ++Persona.contadorPersonas;
+    }
+
+    // Se ejecuta al crear un objeto de la clase
+    {
+        System.out.println("Ejecucion Bloque no estatico o dinámico");
+        this.idPersona = Persona.contadorPersonas++;
+    }
+
+    public Persona(){
+        System.out.println("Ejecución del constructor de la clase");
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Persona{");
+        sb.append("idPersona=").append(idPersona);
+        sb.append('}');
+        return sb.toString();
+    }
+}
