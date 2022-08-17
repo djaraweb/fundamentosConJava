c48d34c5ae32a7cb86b9c73d1abf29cb7339f3aa
Deyvi E. Jara Garcia <deyvi.jara@rappi.com>
2022-08-16 21:50:35 -0500

    Programacion - Sobreescritura y polimorfirmo

---
+++ src/main/java/com/udemy/programacion/sobreescritura/Empleado.java
@@ -0,0 +1,31 @@
+package com.udemy.programacion.sobreescritura;
+
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+
+    public Empleado(String nombre, Double sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public String obtenerDetalles(){
+        return "Nombre: " + this.nombre + ", sueldo: " + this.sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+}
---
+++ src/main/java/com/udemy/programacion/sobreescritura/TestSobreescritura.java
@@ -0,0 +1,20 @@
+package com.udemy.programacion.sobreescritura;
+
+public class TestSobreescritura {
+
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 2500.);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        imprimir(empleado1);
+
+        Gerente gerente1 = new Gerente("David", 1500., "Piura");
+        //System.out.println("gerente1: " + gerente1.obtenerDetalles());
+        imprimir(gerente1);
+    }
+
+    public static void imprimir(Empleado empleado) {
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+
+    }
+
+}
